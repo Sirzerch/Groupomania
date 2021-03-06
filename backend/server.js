29f498488d5c38595181e46a9f6047f80e088887
Sirzerch <eniquentind@gmail.com>
2021-03-06 14:53:12 +0100

    Ajouts des headers et méthodes

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,14 @@ const likeRoutes = require('./routes/like');
 dotenv.config({ path: './config/.env' });
 const app = express();
 
+//Configure et autorise les requêtes Multi-Origin; définit les Headers & les Methodes
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 //bodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
